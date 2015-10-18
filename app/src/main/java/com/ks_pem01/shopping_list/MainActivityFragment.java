4ce653e0c8a846f20580c85f551d3e53267854d8
meggriffin <katrinschauer@gmx.de>
2015-10-18 16:59:54 +0200

    Shopping List with 5 Items and images

---
+++ app/src/main/java/com/ks_pem01/shopping_list/MainActivityFragment.java
@@ -0,0 +1,22 @@
+package com.ks_pem01.shopping_list;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
